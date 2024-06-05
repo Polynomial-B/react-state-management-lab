2921cd951bd2db911744b7a4c1d8fb750996d230
Matt <mphlamb@gmail.com>
2024-06-05 16:20:08 +0200

    Zombie.jsx added as component First map function added Styling for mapped elements added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css'
+import Zombie from './components/Zombie.jsx'
 
 const App = () => {
 
   return (
-    <h1>Hello world!</h1>
+    <Zombie />
   );
 }
 
